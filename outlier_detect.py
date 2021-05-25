2b40ec2db941f4e974b1d75afe107fa1e2dab605
Felix Pei <felix@ilinke.com>
2021-05-25 17:53:19 +0800

    Update outlier_detect.py

--- outlier_detect.py
+++ outlier_detect.py
@@ -2,7 +2,6 @@
 Outlier detection
 """
 
-#%%
 from numpy import random
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,5 +11,10 @@ import numpy as np
 random_data = np.random.randn(50000) * 20 + 20
 sns.boxplot(data=random_data)
 
+#大於UpLimit or 小於DownLimit的值為outlier
+percenttile_arr = np.percentile(random_data, [0, 25, 50, 100])
+IQR = percenttile_arr[3] - percenttile_arr[1]
+UpLimit = percenttile_arr[3]+IQR*1.5
+DownLimit = percenttile_arr[1]-IQR*1.5
 
 # %%
