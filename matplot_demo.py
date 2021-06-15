4d2b27dd8f0a772299d0a51495e7ff84c7464265
Felix Pei <felix@ilinke.com>
2021-06-15 22:30:38 +0800

    Add files via upload
    更多範例請參考
    # Top 50 matplotlib Visualizations – The Master Plots (with full python code)
    #  https://www.machinelearningplus.com/top-50-matplotlib-visualizations-the-master-plots-python/#3ranking

---
+++ matplot_demo.py
@@ -0,0 +1,69 @@
+
+# %%
+
+# Deviation
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Prepare Data
+df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mtcars.csv")
+x = df.loc[:, ['mpg']]
+
+# 新增mpg的zscore欄位 (轉成zscore後可讓不同類型的統計資料進行比較)
+df['mpg_z'] = (x - x.mean())/x.std()
+df['colors'] = ['red' if x < 0 else 'green' for x in df['mpg_z']]
+df.sort_values('mpg_z', inplace=True)
+df.reset_index(inplace=True)
+
+# Draw plot
+plt.figure(figsize=(14, 10), dpi=80)
+plt.hlines(y=df.index, xmin=0, xmax=df.mpg_z, color=df.colors, alpha=0.4, linewidth=5)
+
+# Decorations
+plt.gca().set(ylabel='$Model$', xlabel='$Mileage$')
+plt.yticks(df.index, df.cars, fontsize=12)
+plt.title('Diverging Bars of Car Mileage', fontdict={'size': 20})
+plt.grid(linestyle='--', alpha=0.5)
+plt.show()
+
+
+# %%
+# Ranking
+
+# Prepare Data
+df_raw = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
+df = df_raw[['cty', 'manufacturer']].groupby('manufacturer').apply(lambda x: x.mean())
+df.sort_values('cty', inplace=True)
+df.reset_index(inplace=True)
+
+# Draw plot
+import matplotlib.patches as patches
+
+fig, ax = plt.subplots(figsize=(16, 10), facecolor='white', dpi=80)
+ax.vlines(x=df.index, ymin=0, ymax=df.cty, color='firebrick', alpha=0.7, linewidth=20)
+
+# Annotate Text
+for i, cty in enumerate(df.cty):
+    ax.text(i, cty+0.5, round(cty, 1), horizontalalignment='center')
+
+
+# Title, Label, Ticks and Ylim
+ax.set_title('Bar Chart for Highway Mileage', fontdict={'size': 22})
+ax.set(ylabel='Miles Per Gallon', ylim=(0, 30))
+plt.xticks(df.index, df.manufacturer.str.upper(), rotation=60, horizontalalignment='right', fontsize=12)
+
+# Add patches to color the X axis labels
+p1 = patches.Rectangle((.57, -0.005), width=.33, height=.13, alpha=.1, facecolor='green', transform=fig.transFigure)
+p2 = patches.Rectangle((.124, -0.005), width=.446, height=.13, alpha=.1, facecolor='red', transform=fig.transFigure)
+fig.add_artist(p1)
+fig.add_artist(p2)
+plt.show()
+
+# %%
+
+# 更多範例請參考
+# Top 50 matplotlib Visualizations – The Master Plots (with full python code)
+# https://www.machinelearningplus.com/top-50-matplotlib-visualizations-the-master-plots-python/#3ranking
